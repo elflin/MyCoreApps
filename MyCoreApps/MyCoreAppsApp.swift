927a78bc4b1113cea3bf1957b1bb71a15a60450e
Evan Tanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2024-05-06 13:27:13 +0700

    Initial Commit

---
+++ MyCoreApps/MyCoreAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoreAppsApp.swift
+//  MyCoreApps
+//
+//  Created by Evan Tanuwijaya on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCoreAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
