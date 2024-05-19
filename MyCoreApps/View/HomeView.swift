b63141812ab8f41385b087e2b3aa9e480ce206c0
Evan Tanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2024-05-19 13:43:16 +0700

    Add destionation

--- MyCoreApps/View/HomeView.swift
+++ MyCoreApps/View/HomeView.swift
@@ -12,24 +12,35 @@ struct HomeView: View {
     @Environment(\.modelContext) var modelContext
     
     @Query var destinations: [Destination]
+    @State private var path = [Destination]()
     
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 ForEach(destinations) { destination in
-                    VStack(alignment: .leading) {
-                        Text(destination.name)
-                            .font(.headline)
-
-                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    NavigationLink(value: destination) {
+                        VStack(alignment: .leading) {
+                            Text(destination.name)
+                                .font(.headline)
+                            
+                            Text(destination.date.formatted(date: .long, time: .shortened))
+                        }
                     }
                 }.onDelete(perform: deleteDestinations)
             }
             .navigationTitle("iTour")
+            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
             .toolbar {
                 Button("Add Samples", action: addSamples)
+                Button("Add Destination", systemImage: "plus", action: addDestination)
             }
         }
+    }
+    
+    func addDestination() {
+        let destination = Destination()
+        modelContext.insert(destination)
+        path = [destination]
     }
     
     func addSamples() {
